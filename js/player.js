777f5961feac6e46d3e224513c9bcee228a1cf64
Bloody-men <44526256+Bloody-men@users.noreply.github.com>
2019-02-26 08:50:21 +0800

    Update player.js

--- js/player.js
+++ js/player.js
@@ -7,10 +7,10 @@ $("button").click(function(){
     $audioBtn.on('click', function () {
         var audio = document.getElementById('music');
         if (audio.paused) {
-            audio.play();
+            audioObj[0].play();
             $audioBtn.removeClass('off').addClass('on').find('div').addClass('rotate');
         } else {
-            audio.pause();
+            audioObj[0].pause();
             $audioBtn.removeClass('on').addClass('off').find('div').removeClass('rotate');
         }
     });
